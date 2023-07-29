567e5a3572599fa2ab83f363c26fb5e9a846cdd1
codestation5 <jerry.sandhu@protonmail.com>
2023-07-28 23:14:39 -0700

    create TempMath class and setup base temp entry

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,70 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+
+import java.io. *;
+import java.nio.file.*;
+import java.util.ArrayList;
+import Java.util.List;
+import java.util.Scanner;
+import java.nio.file.StandardOpenOption;
+import java.io.IOException;
+
+public class TempMath {
+    private ArrayList<Temps> temps;
+    private int average;
+    private LocalDateTime todayDate;
+
+public TemperatureMath{
+    todayDate = LocalDateTime.now()
+    this.temp = temp;
+    temps = new ArrayList<Temps>();
+}
+
+        }
+
+
+
+
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    String name x;
+    int tempCount;
+    char choice;
+    File tempLog = new File ("templog.txt");
+    Scanner input = new Scanner(System.in);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        System.out.println "This is a program that takes in daily temperatures, stores them in a txt file and allows you to find the average temperature between chosen days");
+
+        // Check if name has already been entered
+        if (!name) {
+            System.out.println("Enter your name: ")
+            name = input.nextLine();
         }
+        System.out.println ("Hello " + name + ", todays date is: " + TempMath(todayDate))
+        System.out.print("Enter in todays temperature in Celcius: ")
+        String userTemp = input.nextLine();
+        try {
+            Files.write(tempLog.toPath(), Lines, StandardOpenOption.CREATE, StandardOpenOption.APPEND); )
+        }
+        catch (IOException ex) {
+            System.out.println ("Error: unable to write to file: " + ex.getMessage())
+        }
+        tempCount++;
+
+        TempMath tempMath = TempMath(userTemp);
+        if (tempCount > 1) {
+            System.out.println("Do you wish to find the average of a range of temperatures? Enter Y for yes, anything else for no")
+            if (choice == 'Y' || choice == 'y')
+
+        }
+
+        List<String>  lines = new ArrayList<>();
+
+
+
+
+
     }
 }
